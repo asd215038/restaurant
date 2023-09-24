eb292e7c3d1134c9aa79c67eda6dfe6ea0dff150
asd215038 <asd215038@gmail.com>
2023-09-25 01:17:03 +0800

    register model

--- rest/admin.py
+++ rest/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Origins, Origins_Food, Processing_Plant, Product, Booking
 # Register your models here.
+
+admin.site.register(Origins)  
+admin.site.register(Origins_Food)
+admin.site.register(Processing_Plant)
+admin.site.register(Product)
+admin.site.register(Booking)
