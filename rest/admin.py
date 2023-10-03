f22d5830e438d8f827676711ae5c3948c46b732c
asd215038 <asd215038@gmail.com>
2023-10-03 16:19:03 +0800

    feat(admin):將model註冊至admin

--- rest/admin.py
+++ rest/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 from .models import Origins, Origins_Food, Processing_Plant, Product, Booking
 # Register your models here.
 
-admin.site.register(Origins)  
-admin.site.register(Origins_Food)
-admin.site.register(Processing_Plant)
-admin.site.register(Product)
-admin.site.register(Booking)
+class OriginsAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'name')
+
+class Origins_FoodAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'name', 'produce_from', 'price')
+
+class Processing_PlantAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'name')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('photo', 'name', 'produce_from', 'price')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number_of_people', 'phone_number', 'booking_time', 'create_date')
+
+admin.site.register(Origins, OriginsAdmin)  
+admin.site.register(Origins_Food, Origins_FoodAdmin)
+admin.site.register(Processing_Plant, Processing_PlantAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Booking, BookingAdmin)
