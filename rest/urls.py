58ce0cde76ba808be3ba895552a70ff51595a5f6
asd215038 <asd215038@gmail.com>
2023-10-03 17:15:50 +0800

    feat(urls):新增url

--- rest/urls.py
+++ rest/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views  #引用這個資料夾中的views檔案
 
 urlpatterns = [
-    path('', views.index, name = "Index")
+    path('', views.homepage),
+    path('menu', views.menu),
+    path('ingredients', views.ingredients),
+    path('booking', views.booking),
+
 ]
