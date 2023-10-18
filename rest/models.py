c6dce380dd0d0891c44f6c313778792c2b625e43
asd215038 <asd215038@gmail.com>
2023-10-18 22:41:14 +0800

    feat(model, migrations):makemigrations

--- rest/models.py
+++ rest/models.py
@@ -3,33 +3,33 @@ from django.utils import timezone
 
 #原產地
 class Origins(models.Model):
-    photo = models.ImageField(upload_to='upload/origins/', blank=True, default='img.jpg')
+    photo = models.ImageField(upload_to='origins/', blank=True, default='img.jpg')
     name = models.CharField(max_length=50)
 
 #原料
 class Origins_Food(models.Model):
-    photo = models.ImageField(upload_to='upload/origins_food/', blank=True, default='img.jpg')
-    name = models.CharField(max_length=50)  #品名
+    photo = models.ImageField(upload_to='origins_food/', blank=True, default='img.jpg')
+    name = models.CharField(max_length=50)  
     produce_from = models.ForeignKey(Origins, on_delete=models.CASCADE, blank=True, null=True)
-    price = models.IntegerField()  #價格
+    price = models.IntegerField()
 
 #加工廠
 class Processing_Plant(models.Model):
-    photo = models.ImageField(upload_to='upload/processing_plant/', blank=True, default='img.jpg')
+    photo = models.ImageField(upload_to='processing_plant/', blank=True, default='img.jpg')
     name = models.CharField(max_length=50)
     
 #產品
 class Product(models.Model):
-    photo = models.ImageField(upload_to='upload/product/', blank=True, default='img.jpg')
-    name = models.CharField(max_length=50)  #品名
+    photo = models.ImageField(upload_to='product/', blank=True, default='img.jpg')
+    name = models.CharField(max_length=50)  
     produce_from = models.ForeignKey(Processing_Plant, on_delete=models.CASCADE, blank=True, null=True)
-    price = models.IntegerField()  #價格
+    price = models.IntegerField()
     
 #訂位紀錄
 class Booking(models.Model):
-    name = models.CharField(max_length=50)  #品名
-    number_of_people = models.IntegerField()  #價格
-    phone_number = models.IntegerField()  #價格
-    booking_time = models.DateField(default=timezone.now)  #貼文時間
-    create_date = models.DateField(default=timezone.now)  #貼文時間
+    name = models.CharField(max_length=50)  
+    number_of_people = models.IntegerField()  
+    phone_number = models.IntegerField() 
+    booking_time = models.DateField(default=timezone.now)
+    create_date = models.DateField(default=timezone.now)
     
